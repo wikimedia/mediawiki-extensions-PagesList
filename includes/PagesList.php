05e4f5402aeb555a0bc37e955eebf411379bf41e
Umherirrender <umherirrender_de.wp@web.de>
2025-11-03 21:19:10 +0100

    Use namespaced PHP classes
    Requires MediaWiki 1.40, needed to work with 1.44+ (db47e7f715)
    
    Change-Id: I30de0a0482b3cc9c0d0d660aea5ee0a2406700e4
    (cherry picked from commit a41b2d942614f1109cd484ae7dd3ecf25c2b94dd)

--- includes/PagesList.php
+++ includes/PagesList.php
@@ -1,6 +1,9 @@
 <?php
 
+use MediaWiki\Html\Html;
+use MediaWiki\Linker\Linker;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Title\Title;
 use Wikimedia\Rdbms\IDatabase;
 use Wikimedia\Rdbms\IResultWrapper;
 
